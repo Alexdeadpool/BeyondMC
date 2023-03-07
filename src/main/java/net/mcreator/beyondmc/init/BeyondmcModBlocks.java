dc1ab76a323738523b21bbfda7b3ee76ab04a7e7
Alexdeadpool <alex@video99.co.uk>
2023-03-07 16:49:25 +0000

    light blue redstone lamps added

--- src/main/java/net/mcreator/beyondmc/init/BeyondmcModBlocks.java
+++ src/main/java/net/mcreator/beyondmc/init/BeyondmcModBlocks.java
@@ -22,8 +22,13 @@ import net.mcreator.beyondmc.block.SkyBlock;
 import net.mcreator.beyondmc.block.SilveroreBlock;
 import net.mcreator.beyondmc.block.SilverbricksBlock;
 import net.mcreator.beyondmc.block.RedstonelamponBlock;
+import net.mcreator.beyondmc.block.RedredstonelampBlock;
+import net.mcreator.beyondmc.block.PinkredstonelamponBlock;
+import net.mcreator.beyondmc.block.PinkredstonelampBlock;
 import net.mcreator.beyondmc.block.MethylamineoreBlock;
 import net.mcreator.beyondmc.block.MethglassBlock;
+import net.mcreator.beyondmc.block.LightblueredstonelamponBlock;
+import net.mcreator.beyondmc.block.LightblueredstonelampBlock;
 import net.mcreator.beyondmc.block.IronbricksBlock;
 import net.mcreator.beyondmc.block.DistilledmethblockBlock;
 import net.mcreator.beyondmc.block.CoreBlock;
@@ -32,6 +37,8 @@ import net.mcreator.beyondmc.block.Coke2Block;
 import net.mcreator.beyondmc.block.Coke1Block;
 import net.mcreator.beyondmc.block.Coke0Block;
 import net.mcreator.beyondmc.block.CocainblockBlock;
+import net.mcreator.beyondmc.block.BlueredstonelamponBlock;
+import net.mcreator.beyondmc.block.BlueredstonelampBlock;
 import net.mcreator.beyondmc.BeyondmcMod;
 
 public class BeyondmcModBlocks {
@@ -53,10 +60,16 @@ public class BeyondmcModBlocks {
 	public static final RegistryObject<Block> SILVERORE = REGISTRY.register("silverore", () -> new SilveroreBlock());
 	public static final RegistryObject<Block> IRONBRICKS = REGISTRY.register("ironbricks", () -> new IronbricksBlock());
 	public static final RegistryObject<Block> SILVERBRICKS = REGISTRY.register("silverbricks", () -> new SilverbricksBlock());
-	public static final RegistryObject<Block> REDREDSTONELAMPOFF = REGISTRY.register("redredstonelampoff", () -> new RedredstonelampoffBlock());
 	public static final RegistryObject<Block> REDSTONELAMPON = REGISTRY.register("redstonelampon", () -> new RedstonelamponBlock());
 	public static final RegistryObject<Block> COKE_0 = REGISTRY.register("coke_0", () -> new Coke0Block());
 	public static final RegistryObject<Block> COKE_1 = REGISTRY.register("coke_1", () -> new Coke1Block());
 	public static final RegistryObject<Block> COKE_2 = REGISTRY.register("coke_2", () -> new Coke2Block());
 	public static final RegistryObject<Block> COKE_3 = REGISTRY.register("coke_3", () -> new Coke3Block());
+	public static final RegistryObject<Block> REDREDSTONELAMP = REGISTRY.register("redredstonelamp", () -> new RedredstonelampBlock());
+	public static final RegistryObject<Block> BLUEREDSTONELAMP = REGISTRY.register("blueredstonelamp", () -> new BlueredstonelampBlock());
+	public static final RegistryObject<Block> BLUEREDSTONELAMPON = REGISTRY.register("blueredstonelampon", () -> new BlueredstonelamponBlock());
+	public static final RegistryObject<Block> PINKREDSTONELAMP = REGISTRY.register("pinkredstonelamp", () -> new PinkredstonelampBlock());
+	public static final RegistryObject<Block> PINKREDSTONELAMPON = REGISTRY.register("pinkredstonelampon", () -> new PinkredstonelamponBlock());
+	public static final RegistryObject<Block> LIGHTBLUEREDSTONELAMP = REGISTRY.register("lightblueredstonelamp", () -> new LightblueredstonelampBlock());
+	public static final RegistryObject<Block> LIGHTBLUEREDSTONELAMPON = REGISTRY.register("lightblueredstonelampon", () -> new LightblueredstonelamponBlock());
 }
